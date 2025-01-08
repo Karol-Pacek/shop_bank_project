8b24e17d23bcb5fad5fa8357cf77fb8f8f9720f1
Karol-Pacek <karol.pacek2008@gmail.com>
2025-01-08 14:40:51 +0100

    Update shop.sql

--- shop.sql
+++ shop.sql
@@ -60,7 +60,10 @@ INSERT INTO `products` (`barcode`, `product_name`, `price`) VALUES
 ('5901234123474', 'Mydlo w plynie', 5.90),
 ('5901234123475', 'Plyn do naczyn', 6.80),
 ('5901234123476', 'Papier toaletowy', 14.50),
-('5414635120807', 'Sprezone powietrze', 520.00);
+('5414635120807', 'Sprezone powietrze', 520.00),
+('5449000234636', 'Sprite', 9.00),
+('5449000011527', 'Fanta', 12.00)
+('5449000000996', 'Coca-Cola', 10.00);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
